8b86a0f8fe52821f102a887ac2bbd46f397a7b9b
marcin10lw <marcin10lw@wp.pl>
2022-12-31 13:15:26 +0100

    initial commit

---
+++ src/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, ::before, ::after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: "Plus Jakarta Sans", sans-serif;
+  }
+`;
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    red: "hsl(1, 90%, 64%)",
+    blue: "hsl(219, 85%, 26%)",
+    white: "hsl(0, 0%, 100%)",
+    vlGrayishBlue: "hsl(210, 60%, 98%)",
+    lGrayishBlue1: "hsl(211, 68%, 94%)",
+    lGrayishBlue2: "hsl(205, 33%, 90%)",
+    grayishBlue: "hsl(219, 14%, 63%)",
+    dGrayishBlue: "hsl(219, 12%, 42%)",
+    vdGrayishBlue: "hsl(224, 21%, 14%)",
+  },
+  mobile: 767,
+};
